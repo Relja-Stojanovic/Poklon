9a26f62569030db41a20a72328e905bbca2a2fc8
Relja-Stojanovic <reljastojanovic2007@gmail.com>
2026-01-26 00:16:07 +0100

    Visual Fix

--- interface/window_main/board/animation_player.gd
+++ interface/window_main/board/animation_player.gd
@@ -1,9 +1,4 @@
 extends AnimationPlayer
 
-func _on_element_select(_pos: Vector2i) -> void:
-	stop(false)
-	play("test")
-
-func _on_element_cancel() -> void:
-	stop(false)
-	play("RESET")
+func _on_target_select(_pos: Vector2i, _path: PackedVector2Array) -> void:
+	play("target_select")
